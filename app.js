01170fc45a40c4e505e1f4bf508dc7bbe961934e
YsisC <ysisclongart@gmail.com>
2023-06-05 11:23:21 -0400

    web server

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
+require('dotenv').config()
 const express = require('express')
+const hbs = require('hbs');
+
 const app = express()
-const port = 8080
+const port = process.env.PORT
+
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 
 // Servir contenido estatico
 app.use(express.static('public') )
 
-
-app.get('/hola-mundo', (req, res)  =>  {
-  res.send('Hola mundo en su respectiva ruta')
+app.get('/', (req, res)  =>  {
+  res.render('home',{
+    nombre: 'Fernando Herrera',
+    titulo: 'Curso de Node'
+  })
+})
+app.get('/generic', (req, res)  =>  {
+  res.render('generic',{
+    nombre: 'Fernando Herrera',
+    titulo: 'Curso de Node'
+  })
+})
+app.get('/elements', (req, res)  =>  {
+  res.render('elements',{
+    nombre: 'Fernando Herrera',
+    titulo: 'Curso de Node'
+  })
 })
 app.get('*', (req, res) => {
    res.sendFile(__dirname + '/public/404.html')
